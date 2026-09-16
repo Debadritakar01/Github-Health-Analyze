f928bda32b7e507ebb1c9b17571595c59a84dee0
Debadritakar01 <debadritakar01@gmail.com>
2026-09-16 15:34:31 +0530

    intial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <main>
+      <h1>Analyze Your GitHub Repository</h1>
+
+      <p>
+        Enter a GitHub repository URL to analyze its health.
+      </p>
+    </main>
+  );
+}
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <h1>GitHub Repository Health Analyzer</h1>
+    </>
+  );
+}
+
+export default App;
